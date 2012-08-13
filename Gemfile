423a612e4a9174a4d2e9a3584d9a309b7f48b8fb
Jim Sadusky <jim@redguava.com.au>
2012-08-13 12:13:43 +1000

    Updated NavBar

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'thin'
 
 gem 'jquery-rails'
 
+gem 'devise'
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Blog::Application.routes.draw do
+  devise_for :users
+
   match "posts/get_latest" => "posts#get_latest"
 
   resources :posts do
